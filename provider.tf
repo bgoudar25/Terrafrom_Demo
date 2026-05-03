2c18676769c18508d4b92e75fa987e38469cfbb0
Basavaraj <bgoudar25@gmail.com>
2026-05-04 01:01:00 +0530

    test1

--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0388e3ada3d9812da"  # Amazon Linux 2 (example for ap-south-1)
   instance_type = "t3.micro"
-  key_name = "myNVKey"
+  key_name = "MyKeypaire"
 
   tags = {
     Name = "MyTerraformEC2"
