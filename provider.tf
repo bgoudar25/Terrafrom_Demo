5a129b3044b51f16bacb917d74bfa1f0660f3298
Basavaraj <bgoudar25@gmail.com>
2026-05-04 01:14:54 +0530

    test4

--- provider.tf
+++ provider.tf
@@ -11,12 +11,12 @@ resource "aws_instance" "my_ec2" {
     Name = "MyTerraformEC2"
   }
 }
-resource "aws_instance" "my_ec3" {
+resource "aws_instance" "my_ec4" {
   ami           = "ami-0388e3ada3d9812da"  # Amazon Linux 2 (example for ap-south-1)
   instance_type = "t3.micro"
   key_name = "MyKeypaire"
 
   tags = {
-    Name = "MyTerraformEC3"
+    Name = "MyTerraformEC4"
   }
 }
