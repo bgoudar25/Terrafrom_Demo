7d1e5efc603119d4d477f1545049d2bf070414f0
Basavaraj <bgoudar25@gmail.com>
2026-05-04 00:58:11 +0530

    test

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-091138d0f0d41ff90"  # Amazon Linux 2 (example for ap-south-1)
+  ami           = "ami-0388e3ada3d9812da"  # Amazon Linux 2 (example for ap-south-1)
   instance_type = "t3.micro"
   key_name = "myNVKey"
 
