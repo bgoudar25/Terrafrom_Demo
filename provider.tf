5371592d60ef455f224bd853488393111b286dd0
Basavaraj <bgoudar25@gmail.com>
2026-05-04 22:25:31 +0530

    test push

--- provider.tf
+++ provider.tf
@@ -13,12 +13,12 @@ resource "aws_instance" "my_ec2" {
 }
 
 
-resource "aws_instance" "my_ec3" {
+resource "aws_instance" "my_ec5" {
   ami           = "ami-0388e3ada3d9812da"
   instance_type = "t3.micro"
   key_name      = "MyKeypaire"
 
   tags = {
-    Name = "MyTerraformEC3"
+    Name = "MyTerraformEC5"
   }
 }
