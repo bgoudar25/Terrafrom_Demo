c0e16762f844bc3953b56ee1236c7e2d6f711687
Basavaraj <bgoudar25@gmail.com>
2026-05-04 23:03:57 +0530

    changingcode1

--- provider.tf
+++ provider.tf
@@ -13,12 +13,12 @@ resource "aws_instance" "my_ec2" {
 }
 
 
-resource "aws_instance" "my_ec5" {
+resource "aws_instance" "my_ec3" {
   ami           = "ami-0388e3ada3d9812da"
   instance_type = "t3.micro"
   key_name      = "MyKeypaire"
 
   tags = {
-    Name = "MyTerraformEC5"
+    Name = "MyTerraformEC3"
   }
 }
