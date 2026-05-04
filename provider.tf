8814e75ef9bdafcd24f27cb1df1e37eae5b5d0c5
bgoudar25 <bgoudar25@gmail.com>
2026-05-04 22:04:04 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,18 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0388e3ada3d9812da"  # Amazon Linux 2 (example for ap-south-1)
+  ami           = "ami-0388e3ada3d9812da"
   instance_type = "t3.micro"
-  key_name = "MyKeypaire"
-
-  tags = {
-    Name = "MyTerraformEC2"
-  }
-}
-resource "aws_instance" "my_ec2" {
-  ami           = "ami-0388e3ada3d9812da"  # Amazon Linux 2 (example for ap-south-1)
-  instance_type = "t3.micro"
-  key_name = "MyKeypaire"
+  key_name      = "MyKeypaire"
 
   tags = {
     Name = "MyTerraformEC2"
