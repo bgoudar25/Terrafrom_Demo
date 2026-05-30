b116b8a7efc1c5700aae9dd066220b247e06f00c
Basavaraj <bgoudar25@gmail.com>
2026-05-30 12:50:46 +0530

    changed the instance type

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0388e3ada3d9812da"
-  instance_type = "t3.micro"
+  instance_type = "t6.micro"
   key_name      = "MyKeypaire"
 
   tags = {
